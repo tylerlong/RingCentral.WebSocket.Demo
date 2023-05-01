4e8dbfd6d0baf266077debbe5c32a3d3dc9c1470
Tyler Long <tyler4long@gmail.com>
2023-05-01 11:54:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# RingCentral WebSocket Demo
+
+
+## Prerequisites
+
+Make sure that you have created an app in the [RingCentral Developer Portal](https://developer.ringcentral.com/).
+Make sure that this app has permission "WebSocket Subscriptions".
+If you do not know how to do so, please create a new ticket here: https://developers.ringcentral.com/support/create-case
+
+
+## Setup
+
+Rename `.env.sample` to `.env` and edit it to specify your app and user credentials.
+In your C# IDE, configure the project to copy the `.env` file to output.
---
+++ RingCentral.WebSocket.Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using RingCentral;
+using dotenv.net;
+using RingCentral.Net.WebSocket;
+
+var envVars = DotEnv.Read();
+
+var rc = new RestClient(envVars["RINGCENTRAL_CLIENT_ID"], envVars["RINGCENTRAL_CLIENT_SECRET"], envVars["RINGCENTRAL_SERVER_URL"]);
+await rc.Authorize(envVars["RINGCENTRAL_JWT_TOKEN"]);
+Console.WriteLine(rc.token.access_token);
+
+var wsExtension = new WebSocketExtension();
+await rc.InstallExtension(wsExtension);
+await wsExtension.Subscribe(new string[] {"/restapi/v1.0/account/~/extension/~/message-store"}, message =>
+{
+    Console.WriteLine(message);
+});
+
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(60));
+while (await timer.WaitForNextTickAsync())
+{
+    await rc.Restapi().Account().Extension().CompanyPager().Post(new CreateInternalTextMessageRequest
+    {
+        text = "Hello world",
+        from = new PagerCallerInfoRequest
+        {
+            extensionId = rc.token.owner_id
+        },
+        to = new []{ new PagerCallerInfoRequest
+        {
+            extensionId = rc.token.owner_id
+        }}
+    });
+}
+
+await rc.Revoke();
