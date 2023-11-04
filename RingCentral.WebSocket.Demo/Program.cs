3a9b6a4d1f30994c0deffde991c6108d380d5849
Tyler Liu <tyler4long@gmail.com>
2023-11-03 17:11:55 -0700

    Update the demo

--- RingCentral.WebSocket.Demo/Program.cs
+++ RingCentral.WebSocket.Demo/Program.cs
@@ -8,6 +8,13 @@ var envVars = DotEnv.Read();
 var rc = new RestClient(envVars["RINGCENTRAL_CLIENT_ID"], envVars["RINGCENTRAL_CLIENT_SECRET"], envVars["RINGCENTRAL_SERVER_URL"]);
 await rc.Authorize(envVars["RINGCENTRAL_JWT_TOKEN"]);
 
+// install the WebSocket extension
+var wsExtension = new WebSocketExtension(new WebSocketOptions
+{
+    debugMode = true // enable this will print all the WebSocket messages
+});
+await rc.InstallExtension(wsExtension);
+
 // To refresh token every 30 minutes
 // This is necessary, if the token expires, eventually your WebSocket connection will stop working
 // By default token expires every hour, it is perfectly fine to refresh it every 30 minutes. There is no need to to refresh it at a shorter interval
@@ -18,20 +25,16 @@ aTimer.Elapsed += new ElapsedEventHandler(async (object source, ElapsedEventArgs
     {
         await rc.Refresh();
     }
+    // in theory this should not happen, but just in case
     catch
     {
         await rc.Authorize(envVars["RINGCENTRAL_JWT_TOKEN"]);
+        // You will need a new WS session, since you create a new OAuth session
+        await wsExtension.Reconnect();
     }
 });
 aTimer.Interval = 1800000; // 30 minutes
 aTimer.Enabled = true;
-
-// install the WebSocket extension
-var wsExtension = new WebSocketExtension(new WebSocketOptions
-{
-    debugMode = true // enable this will print all the WebSocket messages
-});
-await rc.InstallExtension(wsExtension);
 
 // subscribe to some events
 await wsExtension.Subscribe(new string[] {"/restapi/v1.0/account/~/extension/~/message-store"}, message =>
@@ -40,16 +43,16 @@ await wsExtension.Subscribe(new string[] {"/restapi/v1.0/account/~/extension/~/m
 });
 
 
-// to detect OAuth related issues
-// This may not be necessary, most apps don't need this
-// There is a 5 active oauth sessions per user per app limit
-// If for some particular reasons you need to run 5+ instances, you will need the following code to recover your app
-// You will need it if you often generate 5+ tokens in a short time (maybe for testing purpose?), existing tokens maybe revoked upon 5+ new tokens generated
-// But you really shouldn't run 5+ instances of your app. There is no way to keep them all up and running. This is just a workaround to recover your current instance. 
+// To detect OAuth related issues
+// This is edge case. it should NOT happen in the first place
+// If you encounter this issue, please review the logic of your app and fix it
+// Possible reasons: access token expired or access token  revoked.
+// You should realize that this should not happen at all.
+// So you probably don't need the solution below. Instead, you need a better way to manage you token's lifecycle.
 var reconnecting = false;
-wsExtension.RawMessageReceived += async (sender, str) =>
+wsExtension.MessageReceived += async (sender, wsgMessage) =>
 {
-    if(!reconnecting && str.Contains("\"OAU-")) // some error regarding OAuth, like expired, invalid...etc.
+    if(!reconnecting && wsgMessage.meta.type == MessageType.Error && ((wsgMessage.body.errorCode as string)!).StartsWith("OAU-")) // some error regarding OAuth, like expired, invalid...etc.
     {
         reconnecting = true;
         Console.WriteLine("Token expired/revoked for some reason, I need to reconnect()");
@@ -88,12 +91,6 @@ bTimer.Enabled = true;
 var timer = new PeriodicTimer(TimeSpan.FromMinutes(5));
 while (await timer.WaitForNextTickAsync())
 {
-    // the line below is for testing only, your app doesn't need it
-    // during testing, I create tons of new tokens and existing tokens keep being revoked
-    // so before invoking any API call I need to generate a new token
-    // this is so special case for testing, your app doesn't need it!!!
-    await rc.Authorize(envVars["RINGCENTRAL_JWT_TOKEN"]);
-    
     await rc.Restapi().Account().Extension().CompanyPager().Post(new CreateInternalTextMessageRequest
     {
         text = "Hello world",
